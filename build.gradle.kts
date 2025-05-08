0dc035178012bf41c9d3a48330aeaf4224c8ca8a
anish <anishsapkota054@gmail.com>
2025-05-08 21:14:04 +0200

    init

---
+++ build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    id("dev.nx.gradle.project-graph") version("0.1.0")
+}
+defaultTasks("run")
+
+tasks.register("run") {
+    dependsOn(gradle.includedBuild("my-app").task(":app:run"))
+}
+
+tasks.register("checkAll") {
+    dependsOn(gradle.includedBuild("my-app").task(":app:check"))
+    dependsOn(gradle.includedBuild("my-utils").task(":number-utils:check"))
+    dependsOn(gradle.includedBuild("my-utils").task(":string-utils:check"))
+}
+
+allprojects {
+    apply {
+        plugin("dev.nx.gradle.project-graph")
+    }
+  }
